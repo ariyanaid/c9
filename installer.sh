22fc5bea93b936292389146a8ff21f8ed83e732e
ariyanaid <155936822+ariyanaid@users.noreply.github.com>
2025-01-30 19:07:06 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ curl -sL https://deb.nodesource.com/setup_8.x -o ~/nodesource_setup.sh
 sudo bash ~/nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo ufw allow 8080
-git clone https://github.com/c9/core.git ~/c9sdk
-sudo bash ~/c9sdk/scripts/install-sdk.sh
+git clone https://github.com/c9/core.git ~/c9
+sudo bash ~/c9/scripts/install-sdk.sh
 sudo apt-get install npm -y
 npm install forever -g
